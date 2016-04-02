cbc76d9e346f43fbbe0206c42c9326fcc4503b78
Pete Nelson <pete@petenelson.com>
2016-04-02 15:50:56 -0500

    added tests for the dashboard-directory-size-get filter

--- tests/test-dashboard-directory-size-common.php
+++ tests/test-dashboard-directory-size-common.php
@@ -14,25 +14,6 @@ class Test_Dashboard_Directory_Size_Common extends WP_UnitTestCase {
 		parent::setUp();
 	}
 
-	function test_class_exists() {
-		$this->assertTrue( class_exists( 'Dashboard_Directory_Size_Common') );
-	}
-
-	function test_methods_exist() {
-
-		$common = new Dashboard_Directory_Size_Common();
-
-		$methods = array(
-			'plugins_loaded',
-			'add_transient_flushers',
-			);
-
-		foreach( $methods as $method ) {
-			$this->assertTrue( method_exists( $common, $method ), $method . ' does not exist' );
-		}
-
-	}
-
 	function test_filter_get_valid_directory_size() {
 		$size = apply_filters( Dashboard_Directory_Size_Common::PLUGIN_NAME . '-get', 0, ABSPATH . 'wp-includes' );
 		$this->assertGreaterThan( 0, $size, 'valid paths should return greater than 0' );
