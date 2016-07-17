ea1af5e0dfaa14b8f2f02b8eb8d056a23780410e
Pete Nelson <pete@petenelson.com>
2016-07-17 18:10:37 -0500

    added test_plugins_loaded

--- tests/test-settings.php
+++ tests/test-settings.php
@@ -12,4 +12,17 @@ class Test_Dashboard_Directory_Size_Settings extends Test_Dashboard_Directory_Si
 		M::tearDown();
 	}
 
+	public function test_plugins_loaded() {
+
+		M::expectActionAdded( 'admin_init',      'Dashboard_Directory_Size_Settings::admin_init' );
+		M::expectActionAdded( 'admin_menu',      'Dashboard_Directory_Size_Settings::admin_menu' );
+		M::expectActionAdded( 'admin_notices',   'Dashboard_Directory_Size_Settings::activation_admin_notice' );
+
+		M::expectFilterAdded( Dashboard_Directory_Size_Common::PLUGIN_NAME . '-setting-is-enabled', 'Dashboard_Directory_Size_Settings::setting_is_enabled', 10, 3 );
+		M::expectFilterAdded( Dashboard_Directory_Size_Common::PLUGIN_NAME . '-setting-get', 'Dashboard_Directory_Size_Settings::setting_get', 10, 3 );
+
+		Dashboard_Directory_Size_Settings::plugins_loaded();
+
+	}
+
 }
